72f422ca2bd87889e1950e6ddd70a4c32a681132
Chris Tsang <christopher.tsang@uconn.edu>
2022-03-03 13:39:49 -0500

    added validation utils

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,15 +10,16 @@ const router = express.Router();
 router.post(
     '/',
     asyncHandler(async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
-  
-      await setTokenCookie(res, user);
-  
-      return res.json({
-        user
-      });
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
     })
-  );
+);
+
 
 module.exports = router;
