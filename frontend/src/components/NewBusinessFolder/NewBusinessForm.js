0447a158d8bd4d882502628e7d57f94f2c1d10bf
Chris Tsang <christopher.tsang@uconn.edu>
2022-03-10 18:38:46 -0500

    validations new form

--- frontend/src/components/NewBusinessFolder/NewBusinessForm.js
+++ frontend/src/components/NewBusinessFolder/NewBusinessForm.js
@@ -1,9 +1,10 @@
 import { useDispatch } from "react-redux"
 import { postBusiness } from "../../store/business"
-import { useState, } from "react";
+import { useEffect, useState, } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import './NewForm.css'
+
 
 
 const NewBusinessForm = () => {
@@ -14,6 +15,8 @@ const NewBusinessForm = () => {
     // const nextBusiness = useSelector(state => state.businessState);
     // console.log(nextBusiness)
     //console.log(sessionUser?.id)
+    const [errors, setValidationErrors] = useState([])
+
 
     //const [ownerId, setOwnerId] = useState(sessionUser)
     const [id, setId] = useState('')
@@ -21,12 +24,23 @@ const NewBusinessForm = () => {
     const [description, setDescription] = useState('')
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
-    const [state, setState] = useState('Alabama');
+    const [state, setState] = useState('');
     const [zipCode, setZipCode] = useState('');
     //will have to update model
     //country?
     //phone?
     //many locations?
+
+    useEffect(() => {
+        const errors = []
+        if (!title) errors.push('Name of business required')
+        if (!description) errors.push('Description of business required')
+        if (!address) errors.push('Address of business required')
+        if (!city) errors.push('City required')
+        if (!zipCode) errors.push('Zip code required')
+
+        setValidationErrors(errors)
+    }, [title,description,address,city,zipCode])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -40,7 +54,7 @@ const NewBusinessForm = () => {
             state,
             zipCode,
         }
-        
+
 
         const newBusiness = await dispatch(postBusiness(business))
         console.log(newBusiness)
@@ -53,104 +67,109 @@ const NewBusinessForm = () => {
     return (
         <div className="new-form-large-container">
             <div className="left-half">
-            <div><h1>New Business Form</h1></div>
-            
-            <form className="new-business-form" onSubmit={handleSubmit}>
-                <input
-                type='hidden'
-                name='ownerId'
-                ></input>
-                <input
-                type='hidden'
-                name='businessId'
-                value={id}>
-                </input>
-                <input
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                    placeholder='Name of Business'
-                    name='title' />
-                <textarea
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                    placeholder='Description'
-                    name='description' />
-                <input
-                    onChange={(e) => setAddress(e.target.value)}
-                    value={address}
-                    placeholder='Address'
-                    name='address' />
-                <input
-                    onChange={(e) => setCity(e.target.value)}
-                    value={city}
-                    placeholder='City'
-                    name='city' />
-                <label>State:</label>
-                <select
-                    onChange={(e) => setState(e.target.value)}
-                    value={state}
-                    name='state'
-                >
-                    <option>--Select State--</option>
-                    <option value="AL">Alabama</option>
-                    <option value="AK">Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
-                </select>
-                <input
-                    onChange={(e) => setZipCode(e.target.value)}
-                    value={zipCode}
-                    placeholder='Zip Code'
-                    name='zipCode' />
-                <button type="submit">Submit</button>
-            </form>
+                <div><h1>New Business Form</h1></div>
+
+                <form className="new-business-form" onSubmit={handleSubmit}>
+                    <ul className="errors">
+                        {errors.map(error => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                    <input
+                        type='hidden'
+                        name='ownerId'
+                    ></input>
+                    <input
+                        type='hidden'
+                        name='businessId'
+                        value={id}>
+                    </input>
+                    <input
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                        placeholder='Name of Business'
+                        name='title' />
+                    <textarea
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                        placeholder='Description'
+                        name='description' />
+                    <input
+                        onChange={(e) => setAddress(e.target.value)}
+                        value={address}
+                        placeholder='Address'
+                        name='address' />
+                    <input
+                        onChange={(e) => setCity(e.target.value)}
+                        value={city}
+                        placeholder='City'
+                        name='city' />
+                    <label>State:</label>
+                    <select
+                        onChange={(e) => setState(e.target.value)}
+                        value={state}
+                        name='state'
+                    >
+                        <option>--Select State--</option>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </select>
+                    <input
+                        onChange={(e) => setZipCode(e.target.value)}
+                        value={zipCode}
+                        placeholder='Zip Code'
+                        name='zipCode' />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
             <div className="right-half"></div>
 
