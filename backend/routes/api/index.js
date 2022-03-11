73d091d9704e00e2a175c1d3e0fb197b81ee474b
Chris Tsang <christopher.tsang@uconn.edu>
2022-03-11 01:07:16 -0500

    can get specific reviews and all reviews load in redux state

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const businessRouter = require('./businesses.js')
+const reviewsRouter = require('./reviews')
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/business', businessRouter)
+router.use('/business', businessRouter);
+
+router.use('/reviews', reviewsRouter)
 
 
 
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,9 +7,25 @@ const router = express.Router();
 
 //get all reviews
 router.get(
-    '/reviews',
+    '/allReviews',
     asyncHandler(async function (req, res) {
         const reviews = await Review.findAll();
         res.json(reviews);
     })
 );
+
+//get all reviews for one business
+router.get(
+    '/:id',
+    asyncHandler(async function (req, res) {
+        const businessId = +req.params.id
+        const reviews = await Review.findAll({
+            where: {
+                businessId: businessId
+            }
+        })
+        return res.json(reviews);
+    })
+)
+
+module.exports = router;
