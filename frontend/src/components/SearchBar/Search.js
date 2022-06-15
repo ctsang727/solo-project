aaa243d262f80bb0a74d7e39de34ab9c7771b4b7
Chris Tsang <christopher.tsang@uconn.edu>
2022-06-15 14:50:17 -0400

    search done

---
+++ frontend/src/components/SearchBar/Search.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import './search.css'
+
+function Search({ placeholder, data }) {
+  console.log('data', data)
+  const newData = Object.values(data)
+  const [filteredData, setFilteredData] = useState([])
+  const [wordEntered, setWordEntered] = useState([])
+  console.log('FFFF', filteredData)
+
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    setWordEntered(searchWord)
+    const newFilter = newData.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase())
+    })
+    if (searchWord === '') {
+      setFilteredData([])
+    } else {
+      setFilteredData(newFilter);
+    }
+  }
+
+  const clearInput = () => {
+    setFilteredData([])
+    setWordEntered('');
+  }
+  // value={wordEntered}
+  //onClick={clearInput} 
+  // }
+
+  return (
+    <div className='search'>
+      <div className='searchInputs'>
+        <input type='text' value={wordEntered} placeholder='Search' onChange={handleFilter}></input>
+        {filteredData.length === 0 && wordEntered.length === 0 &&
+          <button id='searchbar-icon'><i class="fa-solid fa-magnifying-glass"></i></button>
+        }
+        {filteredData.length > 0 &&
+          <button onClick={clearInput} id='searchbar-icon'><i class="fa-solid fa-xmark"></i></button>
+        }
+        {wordEntered.length > 0 && filteredData.length === 0 &&
+          <button onClick={clearInput} id='searchbar-icon'><i class="fa-solid fa-xmark"></i></button>
+        }
+      </div>
+      {wordEntered.length > 0 && filteredData.length === 0 &&
+        <div className='dataResult'>
+          <a className='dataItem' target='blank'>
+            <p> No Results </p>
+          </a>
+        </div>
+      }
+
+      {filteredData.length !== 0 &&
+        <div className='dataResult'>
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className='dataItem' href={`/business/${value.id}`} target='blank'>
+                <p> {value.title}</p>
+              </a>
+            )
+          })}
+        </div>
+      }
+      <div className='splash-under-search'>
+        <div id='under-search'>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Express</li>
+          <li>Sequelize</li>
+          <li>Javascript</li>
+        </div>
+      </div>
+    </div>
+
+  )
+
+}
+
+export default Search
